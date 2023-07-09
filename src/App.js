bb9d323ccbb5c917f303903d04a348c5f93aedd1
ScriptSavvy <90902206+Divi202@users.noreply.github.com>
2023-07-09 21:04:12 +0530

    remove link from the router

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import Alert from "./components/Alert";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
