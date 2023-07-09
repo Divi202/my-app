ac85689d2faa6f6103b83db007f8c22e9222a1ee
ScriptSavvy <90902206+Divi202@users.noreply.github.com>
2023-07-09 20:44:48 +0530

    Created a New Branch for deployment of text util added some changes for deployment modified for removing routers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import { useState } from "react";
 import "./App.css";
-import About from "./components/About";
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import Alert from "./components/Alert";
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
@@ -17,11 +15,11 @@ function App() {
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
-      showAlert("Light Mode enabled", "success");
+      showAlert("Light Mode enabled","success");
     }
   };
   const [alert, setAlert] = useState(null);
-  const showAlert = (message, type) => {
+  const showAlert = (message, type)=>{
     setAlert({
       msg: message,
       type: type,
@@ -32,32 +30,17 @@ function App() {
   };
   return (
     <>
-      <Router>
-        <Navbar
-          title="TextUtils"
-          mode={mode}
-          toggleMode={toggleMode}
-          About="About"
-        />
-        <Alert alert={alert} />
-
-        <div className="container">
-          <Routes>
-            {/* use exact so that react match the full path if we dont use the exact react match partially */}
-            <Route  exact path="/about" element={<About />} /> 
-            <Route
-               exact path="/"
-              element={
-                <TextForm
-                  showAlert={showAlert}
-                  heading="Enter the text to analyze below"
-                  mode={mode}
-                />
-              }
-            />
-          </Routes>
-        </div>
-      </Router>
+      <Navbar
+        title="TextUtils"
+        mode={mode}
+        toggleMode={toggleMode}
+        About="About"
+      />
+      <Alert alert={alert} />
+      <div className="container">
+        <TextForm  showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
+      </div>
+      {/* <About/> */}
     </>
   );
 }
